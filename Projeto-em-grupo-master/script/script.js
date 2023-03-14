d05287b16c8f7531a16bf9a057ce0b07c0311a12
Larissa Sena <104239291+larissadiasdesena@users.noreply.github.com>
2023-03-14 09:28:22 -0300

    Delete Projeto-em-grupo-master directory

--- Projeto-em-grupo-master/script/script.js
+++
@@ -1,7 +0,0 @@
-function abrirMenu() {
-      document.getElementById("menuOculto").style.width="200px";
- }
- 
- function fecharMenu() {
-   document.getElementById("menuOculto").style.width="0px";
- }
